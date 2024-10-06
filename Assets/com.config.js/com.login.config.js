2b9e3cb0a8e7e4f1e4d5c185acd9990265bf3ea8
devlabscorporation <121150895+uaposalinas@users.noreply.github.com>
2024-10-05 22:31:39 -0600

    just test

--- Assets/com.config.js/com.login.config.js
+++ Assets/com.config.js/com.login.config.js
@@ -9,6 +9,11 @@ const UserName = document.querySelector('.UserName');
 const StoreLogo = document.querySelector('.StoreLogo');
 const StoreName = document.querySelector('.StoreName');
 const StoreID = document.querySelector('.StoreID');
+const ContainerButton = document.querySelector('.ContainerButton');
+
+const AccountInformation = document.querySelector('.AccountInformation');
+const StoreInformation = document.querySelector('.StoreInformation');
+const InitServiceScreen = document.querySelector('.InitServiceScreen');
 
 FormControl.addEventListener('submit', GetDataToAuth);
 
@@ -19,39 +24,48 @@ function GetDataToAuth(Auth){
     const CapturedUserName = document.querySelector('.CapturedUserName').value;
     const CapturedPassword = document.querySelector('.CapturedPassword').value;
 
+    if(CapturedUserName.trim() === '' && CapturedPassword.trim() === ''){
 
-    fetch('../Controllers/com.login.controller.php', {
+        alert('valores vacios')
 
-        method: "POST",
-        headers: {
+    }else{
 
-            "Content-Type": "Application/x-www-form-urlencoded",
+        fetch('../Controllers/com.login.controller.php', {
 
-        },
-        body: `CapturedUserName=${encodeURIComponent(CapturedUserName)}&CapturedPassword=${encodeURIComponent(CapturedPassword)}`,
+            method: "POST",
+            headers: {
+    
+                "Content-Type": "Application/x-www-form-urlencoded",
+    
+            },
+            body: `CapturedUserName=${encodeURIComponent(CapturedUserName)}&CapturedPassword=${encodeURIComponent(CapturedPassword)}`,
+    
+        })
+        .then(response => response.json())
+        .then(Data => {
+    
+           if(Data.access == "true"){
+    
+            WriteLoginData(Data)
+    
+           }else if(Data.access == "false"){
+    
+            const ErrorCode = Data.ErrorCode;
+    
+            alert(ErrorCode)
+    
+           }
+    
+        })
+        .catch(error => {
+    
+            console.warn("Ocurrió un error al procesar la solicitud ErrDescripter: "+error)
+    
+        })
 
-    })
-    .then(response => response.json())
-    .then(Data => {
+    }
 
-       if(Data.access == "true"){
 
-        WriteLoginData(Data)
-
-       }else if(Data.access == "false"){
-
-        const ErrorCode = Data.ErrorCode;
-
-        alert(ErrorCode)
-
-       }
-
-    })
-    .catch(error => {
-
-        console.warn("Ocurrió un error al procesar la solicitud ErrDescripter: "+error)
-
-    })
 
 }
 
@@ -81,5 +95,55 @@ function RestructureStoreID(StoreID){
     const IDKey = `${Region}-${Date}-${PublicKey}`;
 
     return IDKey;
+
+}
+
+
+let FormPosition = 0;
+
+ContainerButton.addEventListener('click', GetCurrentPosition);
+
+function GetCurrentPosition(){
+
+    if(FormPosition == 0){
+
+        AccountInformation.classList.add('HidePage');
+        StoreInformation.classList.add('ShowPageD');
+        ContainerButton.classList.add("BounceIt");
+        ContainerButton.innerHTML = "Iniciar Sesión"
+        FormPosition++
+
+    }else if(FormPosition == 1){
+
+        StoreInformation.classList.add('HidePage');
+        InitServiceScreen.style.display = "flex";
+        ContainerButton.style.display = "none";
+        
+        FormPosition = 0;
+
+        BootService();
+
+    }
+
+}
+
+
+
+
+function BootService(){
+
+    setTimeout(() => {
+
+        document.querySelector('.BootAudio').play();
+        document.querySelector('.limiter').classList.add('HideLogin');
+
+        setTimeout(() => {
+            
+            document.querySelector('.limiter').classList.add('HidePage');
+
+
+        }, 300);
+
+    },3000)
 
 }
